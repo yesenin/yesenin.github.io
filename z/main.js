dc64c64b3c8a06f97d1154ca4fff4a44e0be3079
Anton Yesenin <Anton.Yesenin@dataart.com>
2015-11-16 19:10:33 +0300

    big data + layout

--- z/main.js
+++ z/main.js
@@ -1,5 +1,32 @@
-$.getJSON('http://yesenin.github.io/z/spartak.json', function(data) {
-	for(var i in data) {
-		console.log(data[i][name]);
-	}
+$(document).ready(function() {
+	var viewModel = {
+		rivalList: ko.observableArray()
+	};
+	var scale = 5;
+	var data;
+	$.getJSON('http://yesenin.github.io/z/data.json', function(data) {
+		for(var i in data['rivalList']) {
+			var item = data['rivalList'][i];
+			viewModel.rivalList.push({
+				rival: item['rival'],
+				gamesCount: parseInt(item['data']['wins']) + parseInt(item['data']['loses']) + parseInt(item['data']['draw']) + parseInt(item['data']['goalDraw']),
+				scored:item['data']['we'],
+				scoredWidth: parseInt(item['data']['we']) * scale + 'px',
+				conceded: item['data']['they'],
+				concededWidth: parseInt(item['data']['they']) * scale + 'px',
+				homeConceded: item['data']['theyGuest'],
+				homeScored: item['data']['weHome'],
+				theyGuestWidth: parseInt(item['data']['theyGuest']) * scale + 'px',
+				weHometWidth: parseInt(item['data']['weHome']) * scale + 'px',
+				guestConceded: item['data']['theyHome'],
+				guestScored: item['data']['weGuest'],
+				theyHomeWidth: parseInt(item['data']['theyHome']) * scale + 'px',
+				weGuestWidth: parseInt(item['data']['weGuest']) * scale + 'px',
+				homeResult: [],
+				guestResult: []
+			});
+		}
+	});
+
+	ko.applyBindings(viewModel);
 });
