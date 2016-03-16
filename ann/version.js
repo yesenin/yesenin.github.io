3dddd9576983562132be933bed6526c6e2cf13e4
Anton Yesenin <yesenintoxa@gmail.com>
2016-03-16 23:56:49 +0300

    routing

--- ann/version.js
+++ ann/version.js
@@ -1,4 +1,21 @@
-var app = angular.module('verbs2', []);
+var app = angular.module('verbs2', [
+    'ngRoute'
+  ]);
+
+app.config(function ($routeProvider, $httpProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'main.html',
+            controller: 'MainCtrl'
+        })
+        .when('/add', {
+            templateUrl: 'add.html',
+            controller: 'AddCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+})
 
 app.service('VerbService', function($http) {
     this.get = function() {
@@ -7,6 +24,12 @@ app.service('VerbService', function($http) {
 })
 
 app.controller('MainCtrl', function($scope, VerbService){
+    VerbService.get().then(function(response) {
+        $scope.data = response.data.data;
+    });
+});
+
+app.controller('AddCtrl', function($scope, VerbService){
     VerbService.get().then(function(response) {
         $scope.data = response.data.data;
     });
