a1f46c9b6a2f08c8043026895cc03d991e61e81c
Anton Yesenin <Anton.Yesenin@dataart.com>
2016-03-17 20:38:02 +0300

    beta

--- ann/punishments/main.js
+++ ann/punishments/main.js
@@ -1,39 +1,51 @@
 angular.module('punishments', [])
-.controller('MainCtrl', function($scope){
-    $scope.players = {
-        player1: {name: 'A', points: 0, totalPoints: 0, wordList: [], currentWord: ""},
-        player2: {name: 'B', points: 0, totalPoints: 0, wordList: [], currentWord: ""}};
-    $scope.wordList = [];
+.service('PunishService', function($http) {
+    this.get = function() {
+            var raw = '{"he":{"name":"H","points":0,"totalPoints":0,"wordList":[],"currentWord":""},"she":{"name":"S","points":0,"totalPoints":0,"wordList":[],"currentWord":""},"wordList":[],"history":[]}';
+            //return JSON.parse(raw);
+            return $http.get('http://178.62.58.179/api/punishment');
+        }
+    this.post = function(data) {
+            var raw = '{"he":{"name":"H","points":0,"totalPoints":0,"wordList":[],"currentWord":""},"she":{"name":"S","points":0,"totalPoints":0,"wordList":[],"currentWord":""},"wordList":[],"history":[]}';
+            //return JSON.parse(raw);
+            return $http.post('http://178.62.58.179/api/punishment', data);
+        }
+})
+.controller('MainCtrl', function($scope, PunishService){
+    PunishService.get().then(function(response) {
+        $scope.data = response.data.data[0];
+        $scope.canPunish = function(player) {
+            return !($scope.data[player].currentWord === "" && $scope.data[player].points < 3 && $scope.data.wordList.length > 0);
+        }
+
+        $scope.canComplete = function(player) {
+            return !($scope.data[player].currentWord !== "");
+        }
+    });
     $scope.newWord = "";
-    
     $scope.add = function() {
-        if ($scope.wordList.indexOf($scope.newWord) < 0) {
-            $scope.wordList.push($scope.newWord);
+        if ($scope.newWord === "")
+            return;
+        if ($scope.data.history.indexOf($scope.newWord) < 0) {
+            $scope.data.wordList.push($scope.newWord);
+            $scope.data.history.push($scope.newWord);
         }
         $scope.newWord = "";
     }
     
     $scope.punish = function(player) {
-        $scope.players[player].points += 1;
-        $scope.players[player].totalPoints += 1;
-        if ($scope.players[player].points == 3) {
-            var i = Math.floor(Math.random()*$scope.wordList.length);
-            $scope.players[player].currentWord = $scope.wordList[i];
-            $scope.wordList.splice(i, 1);
+        $scope.data[player].points += 1;
+        $scope.data[player].totalPoints += 1;
+        if ($scope.data[player].points == 3) {
+            var i = Math.floor(Math.random()*$scope.data.wordList.length);
+            $scope.data[player].currentWord = $scope.data.wordList[i];
+            $scope.data.wordList.splice(i, 1);
         }
     }
     
     $scope.complete = function(player) {
-        $scope.players[player].wordList.push($scope.players[player].currentWord);
-        $scope.players[player].currentWord = "";
-        $scope.players[player].points = 0;
-    }
-    
-    $scope.canPunish = function(player) {
-        return !($scope.players[player].currentWord === "" && $scope.players[player].points < 3 && $scope.wordList.length > 0);
-    }
-    
-    $scope.canComplete = function(player) {
-        return !($scope.players[player].currentWord !== "");
+        $scope.data[player].wordList.push($scope.data[player].currentWord);
+        $scope.data[player].currentWord = "";
+        $scope.data[player].points = 0;
     }
 });
