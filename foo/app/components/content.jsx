821d8b0ca1ea7032983d30d4fdd64e51209faca9
Anton Yesenin <Anton.Yesenin@dataart.com>
2017-01-18 16:31:39 +0300

    axois

--- foo/app/components/content.jsx
+++ foo/app/components/content.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import axios from 'axios';
+
+class SeasonItem extends React.Component {
+  render() {
+    return <li>{this.props.title}</li>
+  }
+}
+
+class SeasonList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      seasons: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://yesenin.github.io/yepl/seasons.json')
+      .then(response => {
+        const foo = response.data.seasons;
+        this.setState({seasons: response.data.seasons});
+      });
+  }
+  
+  render() {
+    const items = this.state.seasons.map((season) => <SeasonItem key={season} title={season} />);
+    return <ul>{items}</ul>
+  }
+}
 
 class ContentComponent extends React.Component {
+  
 
   render() {
       return <main>
       <article>
         <div className="caption">Season tables</div>
-        <div className="meta">meta</div>
-        <div className="text">text</div>
+        <div className="text"><SeasonList /></div>
         </article>
       </main>;
   }
