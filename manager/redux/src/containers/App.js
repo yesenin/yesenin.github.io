063a8663185352344e0cad983d70546bdebc1a08
Anton Yesenin <Anton.Yesenin@dataart.com>
2017-06-02 20:46:00 +0300

    svg

--- manager/redux/src/containers/App.js
+++ manager/redux/src/containers/App.js
@@ -4,25 +4,39 @@ import { saveDirectory, selectDirectory, deleteDirectory } from '../actions/dire
 import { saveNote } from '../actions/noteActions'
 import Tree from '../components/TreeContainer'
 
+import logo from '../assets/logo.svg'
+import Radium from 'radium'
+import styles from '../styles'
+
 class App extends Component {
   render() {
     return (
-      <div>
         <div>
-          <a href="#" className="menu underline" onClick={this.props.saveDirectory}>Add a folder</a>
-          <a href="#" className="menu underline" onClick={this.props.saveNote}>Add a note</a>
-          <a href="#" className="menu underline" onClick={() => this.props.deleteDirectory(this.props.directories.selectedId)}>Delete selected item</a>
-        </div>
-        <Tree
-          directories={this.props.directories}
-          selectDirectory={this.props.selectDirectory} />
-        <ul>
-            {this.props.notes.all.map((note, i) => <li key={i}>{note.name}</li>)}    
-        </ul>    
+            <header style={[styles.header]}><img src={logo} role="presentation"/></header>  
+            <main style={[styles.main]}>
+                <aside>    
+                    <a href="#" onClick={this.props.saveDirectory}>Add a folder</a>
+                    <a href="#" onClick={this.props.saveNote}>Add a note</a>
+                    <a href="#" onClick={() => this.props.deleteDirectory(this.props.directories.selectedId)}>Delete selected item</a>
+                </aside>
+                <article>    
+                    <Tree
+                    directories={this.props.directories}
+                    selectDirectory={this.props.selectDirectory} />
+                    <ul>
+                        {this.props.notes.all.map((note, i) => <li key={i}>{note.name}</li>)}    
+                    </ul>    
+                </article>
+            </main>
+            <footer style={[styles.footer]}>
+                2017, Anton Yesenin
+            </footer>
       </div>
       )
   }
 }
+
+
 
 const mapProps = (state, ownProps) => {
     return {
@@ -39,5 +53,4 @@ const mapActions = (dispatch) => {
         saveNote: () => dispatch(saveNote({name: 'New note'}))
     }
 }
-
-export default connect(mapProps, mapActions)(App)
+export default connect(mapProps, mapActions)(Radium(App))
---
+++ manager/redux/src/styles/index.js
@@ -0,0 +1,17 @@
+export default {
+    header: {
+        textAlign: 'center',
+        margin: '10px 0'
+    },
+    main: {
+        display: 'flex'
+    },
+    footer: {
+        fontFamily: 'serif',
+        fontSize: '14pt',
+        borderTop: '1px solid black',
+        width: '100%',
+        height: '40px',
+        padding: '10px 5px'
+    }
+}
