2e4044f9670f9112c0e19b522008f6d689ea871f
Anton Yesenin <yesenintoxa@gmail.com>
2018-01-06 15:43:20 +0300

    Clean up

--- manager/redux/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import directories from './directoriesReducer'
-import notes from './notesReducer'
-
-export default combineReducers({
-  directories,
-  notes
-})
