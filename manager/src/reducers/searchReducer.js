a1435f59f3553a8fe17884aa3aff8b37cd3122f5
Anton Yesenin <Anton.Yesenin@dataart.com>
2018-02-09 13:12:53 +0300

    searchReducer

---
+++ manager/src/reducers/searchReducer.js
@@ -0,0 +1,17 @@
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
+
+const searchReducer = (state = initialState.notes.search, action) => {
+    switch (action.type) {
+        case types.CHANGED_SEARCH_MODE:
+            return Object.assign({}, state, { isAdvanced: action.isAdvanced })
+        case types.CHANGED_SEARCH_QUERY:
+            return Object.assign({}, state, { query: action.query })
+        case types.CHANGED_SEARCH_STATE:
+            return Object.assign({}, state, { isOpen: action.isOpen })
+        default:
+            return state;
+    }
+};
+
+export default searchReducer
