4349d7d1d3dc23b542ed2c3d02fd725b63d8e1eb
Anton Yesenin <anton.yesenin@gmail.com>
2021-07-20 00:09:44 +0300

    Split numbers

--- yesenin-io/src/app.jsx
+++ yesenin-io/src/app.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { foo } from './utils/some';
+import { getRandomInt, splitNumber } from './utils';
 
 export const App = () => {
-    return (
-        <div>yesenin, {foo()}</div>
+    const a = getRandomInt(1, 1000);
+    const d = splitNumber(a);
+    return (<>
+        <div>{a}, {d.map(i => `${i}, `)}</div>
+    </>
     );
 }
