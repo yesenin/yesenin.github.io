de28d17f83e453441abf522b69f85bf5084a26a7
Anton Yesenin <yesenin@Yesenins-MBA.local>
2022-01-20 00:46:11 +0300

    Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { signInWithGoogle } from './firebase/firebase.util';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          А билд то нужно игнорить или нет?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { User } from 'firebase/auth';
+
+import { auth } from './firebase/firebase.util';
+
+interface AppState {
+  currentUser?: User | null;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user: any) => {
+      this.setState({
+        currentUser: user,
+      });
+    })
+  }
+
+  render() {
+    const { currentUser } = this.state;
+
+    return (
+      <div className="App">
+        {
+          currentUser
+            ? <div>
+                <p>{currentUser.displayName}</p>
+                <button onClick={() => auth.signOut()}>Sign out</button>
+              </div>
+            : <button onClick={signInWithGoogle}>Sign in</button>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.util.ts
@@ -0,0 +1,26 @@
+import { FirebaseApp } from '@firebase/app';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const config = {
+    apiKey: "AIzaSyBG88uTin-v_rDJYNAGiKBrrbS9b-i6dnE",
+    authDomain: "yesenin-io.firebaseapp.com",
+    projectId: "yesenin-io",
+    storageBucket: "yesenin-io.appspot.com",
+    messagingSenderId: "737476726205",
+    appId: "1:737476726205:web:f34506c9a5dcca96ce56d4"
+  };
+
+const Firebase: FirebaseApp = firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({prompt: 'select_account'});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default Firebase;
