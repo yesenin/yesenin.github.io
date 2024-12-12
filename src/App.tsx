6c8845cebf8822f64d9aebbaa72637d7ad823548
Anton Yesenin <anton.yesenin@gmail.com>
2024-12-12 19:28:54 +0400

    Vite
    Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React from "react";
-import {Outlet} from "react-router";
-import {Link} from "react-router-dom";
-import styled from "styled-components";
+import { Link } from 'react-router'
 
-const CenteredDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 40px;
-`;
-
-const NavBarDiv = styled.div`
-  margin: 5px;
-  font-size: 10px;
-`;
-
-const App = () => {
-    return <CenteredDiv>
-        <NavBarDiv>
-            <Link to='/calendar'>Calendar</Link>
-        </NavBarDiv>
-        <Outlet />
-    </CenteredDiv>
+function App() {
+  return (
+    <>
+      <Link to='/calendar'>Calendar</Link>
+    </>
+  )
 }
 
-export default App;
+export default App
